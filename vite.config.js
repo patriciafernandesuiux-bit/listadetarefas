99cb925f512a8f818b43505cdc75489fca711ac3
Patricia <patriciafernades.ui.ux@gmail.com>
2025-10-24 14:17:57 -0300

    Config: Adiciona base path para GitHub Pages no vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  // Adicione esta linha:
+  base: '/listadetarefas/', 
+  plugins: [vue( )],
 })
